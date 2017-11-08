2f220265bb9d6b91fcacc329d13ba6da9ae21f29
Danny Forrest <dannyforrest@gmail.com>
2017-11-08 21:24:39 +0000

    automatically backed up by learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 describe('index.js', function () {
   describe('companyName', function () {
     it('is set as Scuber1234', function () {
-      expect(companyName).to.equal('Scuber');
+      expect(companyName).to.equal('Scuber1');
     });
 
     it('raises error if the companyName is changed', function () {
